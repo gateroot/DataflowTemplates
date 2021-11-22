45985b72eacef7ef74052059e8d28daea867c5ac
sekine <sekine@cloud-ace.jp>
2021-11-22 17:38:34 +0900

    ファイル名にsuffixがつかないようにする

--- src/main/java/com/google/cloud/teleport/templates/CustomTemplateUdfTest.java
+++ src/main/java/com/google/cloud/teleport/templates/CustomTemplateUdfTest.java
@@ -15,42 +15,25 @@
  */
 package com.google.cloud.teleport.templates;
 
-import com.google.api.services.bigquery.model.TableFieldSchema;
-import com.google.api.services.bigquery.model.TableSchema;
-import com.google.cloud.teleport.templates.common.BigQueryConverters;
 import com.google.cloud.teleport.templates.common.JavascriptTextTransformer.JavascriptTextTransformerOptions;
 import com.google.cloud.teleport.templates.common.JavascriptTextTransformer.TransformTextViaJavascript;
-import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import org.apache.beam.runners.dataflow.options.DataflowPipelineOptions;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.coders.StringUtf8Coder;
 import org.apache.beam.sdk.io.Compression;
 import org.apache.beam.sdk.io.FileIO;
-import org.apache.beam.sdk.io.FileIO.ReadableFile;
 import org.apache.beam.sdk.io.FileIO.Write.FileNaming;
 import org.apache.beam.sdk.io.TextIO;
-import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
-import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO.Write.CreateDisposition;
-import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO.Write.WriteDisposition;
 import org.apache.beam.sdk.options.Description;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.options.Validation;
 import org.apache.beam.sdk.options.ValueProvider;
-import org.apache.beam.sdk.options.ValueProvider.NestedValueProvider;
 import org.apache.beam.sdk.transforms.Contextful;
 import org.apache.beam.sdk.transforms.DoFn;
-import org.apache.beam.sdk.transforms.PTransform;
-import org.apache.beam.sdk.transforms.ParDo;
-import org.apache.beam.sdk.transforms.SerializableFunction;
+import org.apache.beam.sdk.transforms.SimpleFunction;
+import org.apache.beam.sdk.transforms.windowing.BoundedWindow;
+import org.apache.beam.sdk.transforms.windowing.PaneInfo;
 import org.apache.beam.sdk.values.KV;
-import org.apache.beam.sdk.values.PCollection;
-import org.apache.beam.sdk.values.POutput;
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -59,8 +42,11 @@ import org.slf4j.LoggerFactory;
  */
 public class CustomTemplateUdfTest {
 
-  /** Options supported by {@link CustomTemplateUdfTest}. */
+  /**
+   * Options supported by {@link CustomTemplateUdfTest}.
+   */
   public interface Options extends DataflowPipelineOptions, JavascriptTextTransformerOptions {
+
     @Description("The GCS location of the text you'd like to process")
     ValueProvider<String> getInputFilePattern();
 
@@ -87,6 +73,7 @@ public class CustomTemplateUdfTest {
   private static final String MODE = "mode";
 
   static class WriteFile extends DoFn<String, byte[]> {
+
     @ProcessElement
     public void processElement(ProcessContext context) throws Exception {
     }
@@ -106,10 +93,23 @@ public class CustomTemplateUdfTest {
                 .build())
         .apply(
             FileIO.<String, KV<String, String>>writeDynamic()
+                .withNumShards(1)
                 .by(KV::getKey)
                 .via(Contextful.fn(KV::getValue), TextIO.sink())
                 .withDestinationCoder(StringUtf8Coder.of())
-                .withNaming(name -> FileIO.Write.defaultNaming(name, ""))
+                .withNaming(new SimpleFunction<String, FileNaming>() {
+                  @Override
+                  public FileNaming apply(String destination) {
+                    return FileIO.Write.relativeFileNaming(
+                        ValueProvider.StaticValueProvider.of(""), new FileNaming() {
+                          @Override
+                          public String getFilename(BoundedWindow window, PaneInfo pane,
+                              int numShards, int shardIndex, Compression compression) {
+                            return destination;
+                          }
+                        });
+                  }
+                })
                 .to("gs://nims-tdm-dataflow-test/data/")
         );
 
